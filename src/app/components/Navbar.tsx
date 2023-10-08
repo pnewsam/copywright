e88afe251fb98a5851e8becf9e2b0833a6a613ee
pnewsam <paul.newsam@gmail.com>
2023-10-08 10:29:50 -0700

    Initial styles.

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export const Navbar = () => (
+  <nav className="w-full flex p-4">
+    <div className="max-w-[1280px] mx-auto w-full">
+      <div>
+        <a className="font-bold text-stone-800" href="/">
+          CopyWright
+        </a>
+      </div>
+    </div>
+  </nav>
+);
